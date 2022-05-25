f09c42895fa02639e4ae877097dbda5e1bb7d6ea
gstok <1157452915@qq.com>
2022-05-25 20:43:45 +0800

    权限

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,9 +1,11 @@
 const getters = {
-  sidebar: state => state.app.sidebar,
-  device: state => state.app.device,
-  token: state => state.user.token,
-  avatar: state => state.user.avatar,
-  name: state => state.user.name,
-  filter: state => state.filter.queryData
+    sidebar: state => state.app.sidebar,
+    device: state => state.app.device,
+    token: state => state.user.token,
+    avatar: state => state.user.avatar,
+    name: state => state.user.name,
+    roles: state => state.user.roles,
+    permission_routes: state => state.permission.routes,
+    filter: state => state.filter.queryData,
 }
 export default getters
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -3,94 +3,119 @@ import { getToken, setToken, removeToken, getOrgId, setOrgId } from '@/utils/aut
 import { resetRouter } from '@/router'
 
 const state = {
-  token: getToken(),
-  name: '',
-  avatar: '',
-  orgId: getOrgId()
+    token: getToken(),
+    name: '',
+    avatar: '',
+    orgId: getOrgId(),
+    roles: []
 }
 
 const mutations = {
-  SET_TOKEN: (state, token) => {
-    state.token = token
-  },
-  SET_NAME: (state, name) => {
-    state.name = name
-  },
-  SET_AVATAR: (state, avatar) => {
-    state.avatar = avatar
-  },
-  SET_ORG_ID: (state, orgId) => {
-    state.orgId = orgId
-  }
+    SET_TOKEN: (state, token) => {
+        state.token = token
+    },
+    SET_NAME: (state, name) => {
+        state.name = name
+    },
+    SET_AVATAR: (state, avatar) => {
+        state.avatar = avatar
+    },
+    SET_ORG_ID: (state, orgId) => {
+        state.orgId = orgId
+    },
+    SET_ROLES: (state, roles) => {
+        state.roles = roles
+    }
 }
 
 const actions = {
-  // user login
-  login({ commit }, userInfo) {
-    const { username, password } = userInfo
-    return new Promise((resolve, reject) => {
-      login({ username: username.trim(), password: password }).then(response => {
-        const { data } = response
-        commit('SET_NAME', data.userName)
-        commit('SET_TOKEN', data.authToken)
-        commit('SET_ORG_ID', data.org.id)
-        setToken(data.authToken)
-        setOrgId(data.org.id)
-        resolve(response)
-      }).catch(error => {
-        reject(error)
-      })
-    })
-  },
+    // user login
+    login({ commit }, userInfo) {
+        const { username, password } = userInfo
+        return new Promise((resolve, reject) => {
+            login({ username: username.trim(), password: password }).then(response => {
+                const { data } = response
+                commit('SET_NAME', data.userName)
+                commit('SET_TOKEN', data.authToken)
+                commit('SET_ORG_ID', data.org.id)
+                setToken(data.authToken)
+                setOrgId(data.org.id)
+                resolve(response)
+            }).catch(error => {
+                reject(error)
+            })
+        })
+    },
 
-  // get user info
-  getInfo ({ commit, state }) {
-    return new Promise((resolve, reject) => {
-      getInfo(state.token).then(response => {
-        const { data } = response
-        if (!data) {
-          reject('Verification failed, please Login again.');
-        }
+    // get user info
+    getInfo({ commit, state }) {
+        return new Promise((resolve, reject) => {
+            getInfo(state.token).then(response => {
+                const { data } = response
+                if (!data) {
+                    reject('Verification failed, please Login again.');
+                }
+                const { roles, userName, avatar } = data
+                commit('SET_ROLES', roles)
+                commit('SET_NAME', userName)
+                commit('SET_AVATAR', avatar)
+                resolve(data)
+            }).catch(error => {
+                reject(error)
+            })
+        })
+    },
 
-        const { userName, avatar } = data
+    // user logout
+    logout({ commit, state }) {
+        return new Promise((resolve, reject) => {
+            logout(state.token).then(() => {
+                commit('SET_TOKEN', '')
+                commit('SET_ROLES', [])
+                removeToken()
+                resetRouter()
+                resolve()
+            }).catch(error => {
+                reject(error)
+            })
+        })
+    },
 
-        commit('SET_NAME', userName)
-        commit('SET_AVATAR', avatar)
-        resolve(data)
-      }).catch(error => {
-        reject(error)
-      })
-    })
-  },
+    // remove token
+    resetToken({ commit }) {
+        return new Promise(resolve => {
+            commit('SET_TOKEN', '')
+            commit('SET_ROLES', [])
+            removeToken()
+            resolve()
+        })
+    },
 
-  // user logout
-  logout({ commit, state }) {
-    return new Promise((resolve, reject) => {
-      logout(state.token).then(() => {
-        commit('SET_TOKEN', '')
-        removeToken()
+    // dynamically modify permissions
+    async changeRoles({ commit, dispatch }, role) {
+        const token = role + '-token'
+
+        commit('SET_TOKEN', token)
+        setToken(token)
+
+        const { roles } = await dispatch('getInfo')
+
         resetRouter()
-        resolve()
-      }).catch(error => {
-        reject(error)
-      })
-    })
-  },
 
-  // remove token
-  resetToken({ commit }) {
-    return new Promise(resolve => {
-      commit('SET_TOKEN', '')
-      removeToken()
-      resolve()
-    })
-  }
+        // generate accessible routes map based on roles
+        const accessRoutes = await dispatch('permission/generateRoutes', roles, { root: true })
+            // dynamically add accessible routes
+        router.addRoutes(accessRoutes)
+
+        // reset visited views and cached views
+        dispatch('tagsView/delAllViews', null, { root: true })
+    }
+
 }
 
 export default {
-  namespaced: true,
-  state,
-  mutations,
-  actions
+    namespaced: true,
+    state,
+    mutations,
+    actions
 }
-
